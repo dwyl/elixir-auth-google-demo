a5d0cef03ba6b64ce5ecb6afb27a77a9b10e7e56
nelsonic <nelson+github@dwyl.io>
2020-04-20 20:57:12 +0100

    remove ecto from test alias in mix.exs

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule App.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["test"]
     ]
   end
 end

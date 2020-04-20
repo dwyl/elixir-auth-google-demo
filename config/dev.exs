9c7e2b00573721016dc159db0a18aedaf5742a17
nelsonic <nelson+github@dwyl.io>
2020-04-20 18:27:25 +0100

    remove webpack watcher from dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -19,16 +19,7 @@ config :app, AppWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  check_origin: false
 
 # ## SSL Support
 #
